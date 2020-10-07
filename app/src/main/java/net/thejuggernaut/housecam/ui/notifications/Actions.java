76608ec56aa2a9d07923214f834f6f9f885642e6
oliverharris <thejuggernaut@harriso.co.uk>
2020-10-07 16:32:56 +0100

    Setup view for settings

---
+++ app/src/main/java/net/thejuggernaut/housecam/ui/notifications/Actions.java
@@ -0,0 +1,11 @@
+package net.thejuggernaut.housecam.ui.notifications;
+
+import android.view.View;
+
+public class Actions {
+
+    public void getSettings(View root){
+        // Get the settings and update the view
+    }
+
+}
