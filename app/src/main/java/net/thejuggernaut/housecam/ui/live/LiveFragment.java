bb10ad76b3ae130c6c4ebf4959a10132dd7959ce
oliverharris <thejuggernaut@harriso.co.uk>
2020-10-20 21:40:20 +0100

    Setup live streaming

--- app/src/main/java/net/thejuggernaut/housecam/ui/live/LiveFragment.java
+++ app/src/main/java/net/thejuggernaut/housecam/ui/live/LiveFragment.java
@@ -13,6 +13,12 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import net.thejuggernaut.housecam.R;
+import net.thejuggernaut.housecam.api.stream.Websocket;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class LiveFragment extends Fragment {
 
@@ -21,6 +27,17 @@ public class LiveFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_live, container, false);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .readTimeout(100,  TimeUnit.MILLISECONDS)
+                .build();
+        Websocket w = new Websocket(getActivity());
+        Request request = new Request.Builder()
+                .url("ws://192.168.1.7:8000/stream")
+                .build();
+        client.newWebSocket(request, w);
+
+        // Trigger shutdown of the dispatcher's executor so this process can exit cleanly.
+        client.dispatcher().executorService().shutdown();
 
         return root;
     }
